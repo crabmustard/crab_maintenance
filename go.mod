8de1cb87c219bb48b8d333841f6a5a1620335a7a
crabmustard <bryce.lippert@gmail.com>
2025-02-09 17:16:36 -0500

    some csv

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/crabmustard/crab_maintenance
 
 go 1.23.3
+
+require github.com/a-h/templ v0.3.819
--- main.go
+++ main.go
@@ -1,8 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/a-h/templ"
+)
+
+const (
+	PORT = ":8080"
+)
+
+type GlobalState struct {
+	Count int
+}
+
+var global GlobalState
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	component := page(global.Count, 0)
+	component.Render(r.Context(), w)
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	if r.Form.Has("global") {
+		global.Count++
+	}
+
+	getHandler(w, r)
+}
+
+type NowHandler struct {
+	Now func() time.Time
+}
+
+func NewNowHandler(now func() time.Time) NowHandler {
+	return NowHandler{Now: now}
+}
+
+func (nh NowHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	timeComponent(nh.Now()).Render(r.Context(), w)
+}
 
 func main() {
-	fmt.Println("Hi")
-	fmt.Println("Hi")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			postHandler(w, r)
+			return
+		}
+		getHandler(w, r)
+	})
+	http.Handle("/time", NewNowHandler(time.Now))
+	http.Handle("/404", templ.Handler(notFoundComponent(), templ.WithStatus(http.StatusNotFound)))
+
+	fmt.Printf("listenning on port %s\n", PORT)
+	http.ListenAndServe(PORT, nil)
 }
