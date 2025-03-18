d28c4654b60e78c3f1e45da60c9a2953aa669ad0
crabmustard <bryce.lippert@gmail.com>
2025-03-18 17:35:50 -0400

    bubbletea is confusing me

--- filldb.go
+++ filldb.go
@@ -1,42 +1,36 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
-	"math/rand"
-	"strconv"
-
 	"github.com/crabmustard/crab_maintenance/database"
 )
 
 func createPtacList(numRooms int64) ([]database.Ptac, error) {
-	brands := []string{"Amana", "HotPoint", "Trane"}
-	models := []string{"12000", "15000", "12000B"}
+	// brands := []string{"amana", "hotpoint", "trane", "distinctions"}
+	// models := []string{"12000", "15000", "12000B"}
 
 	ptacs := []database.Ptac{}
 
-	for room := range int(numRooms) {
-		brandIndex := rand.Intn(3)
-		modelsIndex := rand.Intn(3)
-		year := (2024 - rand.Intn(3))
-		month := rand.Intn(12) + 1
-		day := rand.Intn(30) + 1
-		lastServiceString := fmt.Sprintf("%d-%02d-%02d", year, month, day)
+	// for room := range int(numRooms) {
+	// 	brandIndex := rand.Intn(3)
+	// 	modelsIndex := rand.Intn(3)
+	// 	year := (2024 - rand.Intn(3))
+	// 	month := rand.Intn(12) + 1
+	// 	day := rand.Intn(30) + 1
+	// 	lastServiceString := fmt.Sprintf("%d-%02d-%02d", year, month, day)
 
-		pt := database.CreatePtacParams{
-			Room:        strconv.Itoa(room),
-			Brand:       brands[brandIndex],
-			Model:       models[modelsIndex],
-			LastService: lastServiceString,
-		}
-		ptdb, err := cfg.db.CreatePtac(context.Background(), pt)
-		if err != nil {
-			log.Fatal("unable to add to db")
-		}
-		ptacs = append(ptacs, ptdb)
+	// 	pt := database.CreatePtacParams{
+	// 		Room:        strconv.Itoa(room),
+	// 		Brand:       brands[brandIndex],
+	// 		Model:       models[modelsIndex],
+	// 		LastService: lastServiceString,
+	// 	}
+	// 	ptdb, err := cfg.db.CreatePtac(context.Background(), pt)
+	// 	if err != nil {
+	// 		log.Fatal("unable to add to db")
+	// 	}
+	// 	ptacs = append(ptacs, ptdb)
 
-	}
+	// }
 	return ptacs, nil
 
 }
--- main.go
+++ main.go
@@ -1,87 +1,86 @@
 package main
 
 import (
-	"context"
-	"database/sql"
 	"fmt"
-	"log"
-	"net/http"
 	"os"
 
-	"github.com/a-h/templ"
-	"github.com/crabmustard/crab_maintenance/database"
+	tea "github.com/charmbracelet/bubbletea"
 
-	"github.com/joho/godotenv"
 	_ "modernc.org/sqlite"
 )
 
-type crabConfig struct {
-	port      string
-	filesRoot string
-	db        *database.Queries
-}
+// type crabConfig struct {
+// 	port      string
+// 	filesRoot string
+// 	db        *database.Queries
+// }
 
-var cfg crabConfig
-
-var numRooms int64 = 159
+// var cfg crabConfig
+// var numRooms int64 = 159
 
 func main() {
-	godotenv.Load(".env")
-	cfg.filesRoot = os.Getenv("FILES_ROOT")
-	if cfg.filesRoot == "" {
-		log.Fatal("STUFF_ROOT env variable not set")
-	}
-	cfg.port = os.Getenv("PORT")
-	if cfg.port == "" {
-		log.Fatal("PORT env variable not set")
-	}
-	dbPath := os.Getenv("DB_PATH")
-	db, err := sql.Open("sqlite", dbPath)
-	if err != nil {
-		log.Fatal("error opening db")
-	}
-	dbQueries := database.New(db)
-	cfg.db = dbQueries
 
-	ptacCount, err := cfg.db.GetPtacCount(context.Background())
-	if err != nil {
-		log.Fatal("error with inital ptac count")
+	program := tea.NewProgram(initialPtacModel())
+	if _, err := program.Run(); err != nil {
+		fmt.Printf("Error encountered %v", err)
+		os.Exit(1)
 	}
-	log.Printf("ptac count inital: %d", ptacCount)
+	// godotenv.Load(".env")
+	// cfg.filesRoot = os.Getenv("FILES_ROOT")
+	// if cfg.filesRoot == "" {
+	// 	log.Fatal("STUFF_ROOT env variable not set")
+	// }
+	// cfg.port = os.Getenv("PORT")
+	// if cfg.port == "" {
+	// 	log.Fatal("PORT env variable not set")
+	// }
+	// dbPath := os.Getenv("DB_PATH")
+	// db, err := sql.Open("sqlite", dbPath)
+	// if err != nil {
+	// 	log.Fatal("error opening db")
+	// }
+	// dbQueries := database.New(db)
+	// cfg.db = dbQueries
 
-	err = cfg.db.ClearPtacList(context.Background())
-	if err != nil {
-		log.Fatal("error clearing db")
-	}
+	// ptacCount, err := cfg.db.GetPtacCount(context.Background())
+	// if err != nil {
+	// 	log.Fatal("error with inital ptac count")
+	// }
+	// log.Printf("ptac count inital: %d", ptacCount)
 
-	ptacCount, err = cfg.db.GetPtacCount(context.Background())
-	if err != nil {
-		log.Fatal("error with inital ptac count")
-	}
-	log.Printf("ptac count should be 0: %d", ptacCount)
+	// err = cfg.db.ClearPtacList(context.Background())
+	// if err != nil {
+	// 	log.Fatal("error clearing db")
+	// }
 
-	ptacs, err := createPtacList(numRooms)
-	if err != nil {
-		log.Fatal("error making ptac list")
-	}
+	// ptacCount, err = cfg.db.GetPtacCount(context.Background())
+	// if err != nil {
+	// 	log.Fatal("error with inital ptac count")
+	// }
+	// log.Printf("ptac count should be 0: %d", ptacCount)
 
-	// Checks that the right amount of rooms are in database, move to test later
-	ptacCount, err = cfg.db.GetPtacCount(context.Background())
-	if (err != nil) || (ptacCount != numRooms) {
-		log.Fatal("error with ptac count")
-	}
-	log.Printf("ptac count end: %d", ptacCount)
+	// _, err = createPtacList(numRooms)
+	// if err != nil {
+	// 	log.Fatal("error making ptac list")
+	// }
 
-	mux := http.NewServeMux()
-	mux.Handle("/", templ.Handler(ptacList(ptacs)))
-	stuffHandler := http.StripPrefix("/stuff", http.FileServer(http.Dir(cfg.filesRoot)))
-	mux.Handle("/stuff/", stuffHandler)
+	// // Checks that the right amount of rooms are in database, move to test later
+	// ptacCount, err = cfg.db.GetPtacCount(context.Background())
+	// if (err != nil) || (ptacCount != numRooms) {
+	// 	log.Fatal("error with ptac count")
+	// }
+	// log.Printf("ptac count end: %d", ptacCount)
 
-	srv := &http.Server{
-		Addr:    ":" + cfg.port,
-		Handler: mux,
-	}
+	// mux := http.NewServeMux()
+	// mux.Handle("/", templ.Handler(ptacList(ptacs)))
+	// stuffHandler := http.StripPrefix("/stuff", http.FileServer(http.Dir(cfg.filesRoot)))
+	// mux.Handle("/stuff/", stuffHandler)
 
-	fmt.Printf("listenning on port %s\n", cfg.port)
-	log.Fatal(srv.ListenAndServe())
+	// srv := &http.Server{
+	// 	Addr:    ":" + cfg.port,
+	// 	Handler: mux,
+	// }
+
+	// fmt.Printf("listenning on port %s\n", cfg.port)
+	// log.Fatal(srv.ListenAndServe())
 }
